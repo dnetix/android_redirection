9a407ca873354e31ebdf89a4a08cf19bf19090de
Diego Calle <dnetix@gmail.com>
2016-11-29 13:43:35 -0500

    Displaying settings and log the info

--- app/src/main/java/com/placetopay/dnetix/testingapp/IndexActivity.java
+++ app/src/main/java/com/placetopay/dnetix/testingapp/IndexActivity.java
@@ -1,13 +1,47 @@
 package com.placetopay.dnetix.testingapp;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class IndexActivity extends AppCompatActivity {
+
+    Button testButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.settings, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_settings:
+                Intent i = new Intent(this, SettingsActivity.class);
+                startActivityForResult(i, 1);
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void testing(View v) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        Log.i("appx", sharedPreferences.getString("login", ""));
     }
 }
---
+++ app/src/main/java/com/placetopay/dnetix/testingapp/SettingsActivity.java
@@ -0,0 +1,19 @@
+package com.placetopay.dnetix.testingapp;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SettingsActivity extends PreferenceActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+    }
+
+}
